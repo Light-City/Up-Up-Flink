a66de7c947c03e5c30b42c99693dbede17c40529
light-city <455954986@qq.com>
2019-05-28 18:56:12 +0800

    update hbase

--- README.md
+++ README.md
@@ -5,9 +5,11 @@
 | 目录名称    | 目录描述                                   |
 | ----------- | ------------------------------------------ |
 | [wordcount](./wordcount)   | sock通信，监听端口内的单词统计             |
-| [flink_mysql](./flink_mysql) | Kafka-Flink-Mysql Kafka生成数据并存入Mysql |
-|             |                                            |
+| [flink_mysql](./flink_mysql) | kafka生成数据，flinkkafka消费数据，单条插入Mysql数据库 |
+| [flink_BatchMysql](./flink_BatchMysql) | kafka生成数据，flinkkafka消费数据，批量插入Mysql数据库 |
+| [flink_BatchHBase](./flink_BatchHBase) | kafka生成数据，flinkkafka消费数据，批量插入HBase数据库 |
 
+> 备注：部分学习来源于https://github.com/zhisheng17/flink-learning
 
 ## 关于我
 
---
+++ flink_BatchMysql/src/main/java/mysql/SinkToMySQL.java
@@ -0,0 +1,88 @@
+package mysql;
+
+import model.Student;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+
+public class SinkToMySQL extends RichSinkFunction<List<Student>> {
+    PreparedStatement ps;
+    BasicDataSource dataSource;
+    private Connection connection;
+
+    /**
+     * open() 方法中建立连接，这样不用每次 invoke 的时候都要建立连接和释放连接
+     *
+     * @param parameters
+     * @throws Exception
+     */
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        dataSource = new BasicDataSource();
+        connection = getConnection(dataSource);
+        String sql = "insert into student(id, name, password, age) values(?, ?, ?, ?);";
+        ps = this.connection.prepareStatement(sql);
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+        //关闭连接和释放资源
+        if (connection != null) {
+            connection.close();
+        }
+        if (ps != null) {
+            ps.close();
+        }
+    }
+
+    /**
+     * 每条数据的插入都要调用一次 invoke() 方法
+     *
+     * @param value
+     * @param context
+     * @throws Exception
+     */
+    @Override
+    public void invoke(List<Student> value, Context context) throws Exception {
+        //遍历数据集合
+        for (Student student : value) {
+            ps.setInt(1, student.getId());
+            ps.setString(2, student.getName());
+            ps.setString(3, student.getPassword());
+            ps.setInt(4, student.getAge());
+            System.out.println("id:"+student.getId());
+            ps.addBatch();
+        }
+        int[] count = ps.executeBatch();//批量后执行
+        System.out.println("成功了插入了" + count.length + "行数据");
+    }
+
+
+    private static Connection getConnection(BasicDataSource dataSource) {
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        //注意，替换成自己本地的 mysql 数据库地址和用户名、密码
+        dataSource.setUrl("jdbc:mysql://localhost:3306/tt");
+        dataSource.setUsername("root");
+        dataSource.setPassword("xxxxxx");
+        //设置连接池的一些参数
+        dataSource.setInitialSize(10);
+        dataSource.setMaxTotal(50);
+        dataSource.setMinIdle(2);
+
+        Connection con = null;
+        try {
+            con = dataSource.getConnection();
+            System.out.println("创建连接池：" + con);
+        } catch (Exception e) {
+            System.out.println("-----------mysql get connection has exception , msg = " + e.getMessage());
+        }
+        return con;
+    }
+}
